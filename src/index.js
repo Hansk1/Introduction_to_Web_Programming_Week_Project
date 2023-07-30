62763b662f0b89ac1b80d9fb7e077e51beb16e4e
Hansk1 <101644829+Hansk1@users.noreply.github.com>
2023-07-31 01:41:31 +0300

    Add levelMenu, level2

--- src/index.js
+++ src/index.js
@@ -31,7 +31,7 @@ window.onload = function () {
             },
         },
         parent: "game-container",
-        scene: [MainMenu, Level1, GameOver],
+        scene: [MainMenu, LevelMenu, Level1, Level2, GameOver],
     };
 
     game = new Phaser.Game(gameConfig);
@@ -67,11 +67,9 @@ class Level1 extends Phaser.Scene {
         this.playerHealth = gameOptions.playerHealth;
 
         //Create player:
-        this.player = this.physics.add.sprite(
-            game.config.width / 2,
-            game.config.height / 1.5,
-            "player"
-        );
+        this.player = this.physics.add
+            .sprite(game.config.width / 2, game.config.height / 1.5, "player")
+            .setScale(0.8);
         this.player.body.allowGravity = false;
         this.player.body.drag.set(gameOptions.playerDrag);
         this.player.body.maxVelocity.set(gameOptions.playerMaxSpeed);
@@ -226,7 +224,10 @@ class Level1 extends Phaser.Scene {
         this.healthText.setText(this.playerHealth);
         if (this.playerHealth == 0) {
             console.log("Player died!");
-            this.scene.start("GameOver", { score: this.score });
+            this.scene.start("GameOver", {
+                score: this.score,
+                previousScene: "Level1",
+            });
         }
     }
 
@@ -256,6 +257,7 @@ class GameOver extends Phaser.Scene {
     //Get score from gameScene:
     init(data) {
         this.score = data.score;
+        this.previousScene = data.previousScene;
     }
 
     create() {
@@ -284,14 +286,28 @@ class GameOver extends Phaser.Scene {
         const restartButton = this.add
             .text(
                 game.config.width / 2,
-                game.config.height / 2 + 100,
+                game.config.height / 2 + 150,
                 "Restart",
                 { fontSize: "52px", fill: "#fff" }
             )
             .setOrigin(0.5);
         restartButton.setInteractive();
         restartButton.on("pointerdown", () => {
-            this.scene.start("Level1");
+            this.scene.start(this.previousScene);
+        });
+
+        //LevelMenuBtn:
+        const levelMenuButton = this.add
+            .text(
+                game.config.width / 2,
+                game.config.height / 2 + 100,
+                "Level Menu",
+                { fontSize: "52px", fill: "#fff" }
+            )
+            .setOrigin(0.5);
+        levelMenuButton.setInteractive();
+        levelMenuButton.on("pointerdown", () => {
+            this.scene.start("LevelMenu");
         });
     }
 }
@@ -325,7 +341,284 @@ class MainMenu extends Phaser.Scene {
             .setOrigin(0.5);
         restartButton.setInteractive();
         restartButton.on("pointerdown", () => {
+            this.scene.start("LevelMenu");
+        });
+    }
+}
+
+//LevelMenu scene----------------------------------------------------------------------------------------------------------------
+class LevelMenu extends Phaser.Scene {
+    constructor() {
+        super("LevelMenu");
+    }
+
+    create() {
+        //Choose level text:
+        this.add
+            .text(
+                game.config.width / 2,
+                game.config.height / 2,
+                "Choose level:",
+                {
+                    fontSize: "70px",
+                    fill: "#fff",
+                }
+            )
+            .setOrigin(0.5);
+
+        // Level1 button:
+        const level1Button = this.add
+            .text(
+                game.config.width / 2,
+                game.config.height / 2 + 60,
+                "Level 1",
+                {
+                    fontSize: "36px",
+                    fill: "#fff",
+                }
+            )
+            .setOrigin(0.5);
+        level1Button.setInteractive();
+        level1Button.on("pointerdown", () => {
             this.scene.start("Level1");
         });
+
+        // Level2 button:
+        const level2Button = this.add
+            .text(
+                game.config.width / 2,
+                game.config.height / 2 + 110,
+                "Level 2",
+                {
+                    fontSize: "36px",
+                    fill: "#fff",
+                }
+            )
+            .setOrigin(0.5);
+        level2Button.setInteractive();
+        level2Button.on("pointerdown", () => {
+            this.scene.start("Level2");
+        });
+
+        const mainMenu = this.add
+            .text(
+                game.config.width / 2,
+                game.config.height / 2 + 170,
+                "Main Menu",
+                {
+                    fontSize: "36px",
+                    fill: "#fff",
+                }
+            )
+            .setOrigin(0.5);
+        mainMenu.setInteractive();
+        mainMenu.on("pointerdown", () => {
+            this.scene.start("MainMenu");
+        });
+    }
+}
+
+//Level2 scene----------------------------------------------------------------------------------------------------------------
+class Level2 extends Phaser.Scene {
+    constructor() {
+        super("Level2");
+        this.score = 0;
+        this.playerHealth = gameOptions.playerHealth;
+    }
+
+    preload() {
+        //Preload all used assets:
+        this.load.audio("blastSound", "../sounds/explosion.mp3");
+        this.load.audio("scoreSound", "../sounds/pickupCoin.mp3");
+        this.load.image("player", "../images/player.png");
+        this.load.image("asteroid", "../images/asteroid.png");
+        this.load.image("heart", "../images/heart.png");
+        this.load.image("star", "../images/star.png", {
+            frameWidth: 32,
+            frameHeight: 32,
+        });
+    }
+
+    create() {
+        //Switch background color:
+        this.cameras.main.setBackgroundColor("#242424");
+
+        //Reset starCount:
+        this.score = 0;
+
+        //Reset playerHealth:
+        this.playerHealth = gameOptions.playerHealth;
+
+        //Reset asteroidSpeed:
+        this.asteroidSpeed = gameOptions.asteroidSpeed;
+
+        //Create player:
+        this.player = this.physics.add
+            .sprite(game.config.width / 2, game.config.height / 1.5, "player")
+            .setScale(0.8);
+        this.player.body.allowGravity = false;
+        this.player.body.drag.set(gameOptions.playerDrag);
+        this.player.body.maxVelocity.set(gameOptions.playerMaxSpeed);
+        this.player.body.collideWorldBounds = true;
+
+        //Setups cursors for player input:
+        this.cursors = this.input.keyboard.createCursorKeys();
+
+        //Create asteroid group:
+        this.asteroidGroup = this.physics.add.group({
+            immovable: true,
+            allowGravity: false,
+        });
+
+        //Add triggertimer for spawning asteroids and other game objects:
+        this.triggerTimer = this.time.addEvent({
+            callback: this.createAsteroidRow,
+            callbackScope: this,
+            delay: 2000,
+            loop: true,
+        });
+
+        //Create stargroup:
+        this.starsGroup = this.physics.add.group({
+            immovable: true,
+            allowGravity: false,
+        });
+
+        //Create healthUpGroup:
+        this.healthUpGroup = this.physics.add.group({});
+
+        //Physics colliders:
+        this.physics.add.collider(this.starsGroup, this.asteroidGroup);
+        this.physics.add.collider(this.healthUpGroup, this.asteroidGroup);
+
+        //Physics overlaps:
+        this.physics.add.overlap(
+            this.player,
+            this.asteroidGroup,
+            (player, asteroid) => {
+                this.explosionSound.play();
+                asteroid.destroy();
+                this.endGame();
+            },
+            null,
+            this
+        );
+
+        this.physics.add.overlap(
+            this.player,
+            this.starsGroup,
+            this.collectStar,
+            null,
+            this
+        );
+
+        this.physics.add.overlap(
+            this.player,
+            this.healthUpGroup,
+            this.collectHealthUp,
+            null,
+            this
+        );
+
+        // Create audios:
+        this.scoreSound = this.sound.add("scoreSound");
+        this.explosionSound = this.sound.add("blastSound");
+
+        //Add starIMG and text element top-left of screen for showing score to player:
+        this.add.image(26, 26, "star").setScale(0.06);
+        this.scoreText = this.add.text(50, 10, "0", {
+            fontSize: "40px",
+            fill: "#ffffff",
+        });
+
+        //Health icon and text:
+        this.add.image(26, 70, "heart").setScale(0.6);
+        this.healthText = this.add.text(50, 55, `${this.playerHealth}`, {
+            fontSize: "40px",
+            fill: "#fff",
+        });
+    }
+
+    update() {
+        if (this.cursors.left.isDown) {
+            this.player.body.acceleration.x = -gameOptions.playerAcceleration;
+        } else if (this.cursors.right.isDown) {
+            this.player.body.acceleration.x = gameOptions.playerAcceleration;
+        } else {
+            this.player.body.acceleration.x = 0;
+        }
+
+        if (this.cursors.up.isDown) {
+            this.player.body.acceleration.y = -gameOptions.playerAcceleration;
+        } else if (this.cursors.down.isDown) {
+            this.player.body.acceleration.y = gameOptions.playerAcceleration;
+        } else {
+            this.player.body.acceleration.y = 0;
+        }
+
+        //Way to remove asteroids after they get out of bounds:
+        this.asteroidGroup.children.iterate(function (asteroid) {
+            if (asteroid && asteroid.y > game.config.height + asteroid.height) {
+                asteroid.destroy();
+            }
+        });
+    }
+
+    //Function for creating asteroids:
+    createAsteroidRow() {
+        //Calculate postion for the hole:
+        var rowHole = Math.floor(Math.random() * 7) + 1;
+
+        //Add asteroid to the row:
+        for (let i = 0; i < 9; i++) {
+            if (i != rowHole && i != rowHole + 1) {
+                let asteroid = this.asteroidGroup.create(
+                    i * 70 + 20,
+                    -game.config.height - 50,
+                    "asteroid"
+                );
+                asteroid.setVelocityY(gameOptions.asteroidSpeed);
+            } else if (i === rowHole) {
+                this.starsGroup
+                    .create(i * 70 + 55, -game.config.height - 50, "star")
+                    .setScale(0.06);
+                this.starsGroup.setVelocityY(gameOptions.asteroidSpeed);
+            }
+        }
+
+        //Lower the delay:
+        if (this.triggerTimer.delay > 400) {
+            this.triggerTimer.delay -= 20;
+            console.log("kissa");
+        }
+    }
+
+    //Function for killing player:
+    endGame() {
+        this.playerHealth--;
+        this.healthText.setText(this.playerHealth);
+        if (this.playerHealth == 0) {
+            console.log("Player died!");
+            this.scene.start("GameOver", {
+                score: this.score,
+                previousScene: "Level2",
+            });
+        }
+    }
+
+    //Function for collecting stars:
+    collectStar(player, star) {
+        star.disableBody(true, true);
+        this.score += 1;
+        this.scoreText.setText(this.score);
+        this.scoreSound.play();
+    }
+
+    //Function for collecting healthUps:
+    collectHealthUp(player, healthUp) {
+        healthUp.disableBody(true, true);
+        this.playerHealth += 1;
+        this.healthText.setText(this.playerHealth);
+        this.scoreSound.play();
     }
 }
